e5a23df29ef0ec44cecf2ade849e31a538e97352
Marine14 <marine.favier@hotmail.fr>
2019-10-23 18:15:07 +0200

    2/3 petits arrangements

--- view/categories.view.php
+++ view/categories.view.php
@@ -3,7 +3,6 @@
 <title>Bricomachin</title>
 <meta charset="UTF-8"/>
 <meta http-equiv="content-type" content="text/html;" />
-<meta name="author" content="Jean-Pierre Chevallet" />
 <link rel="stylesheet" type="text/css" href="../view/design/style.css"
 </head>
 
