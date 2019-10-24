f86a0f7a20c73422205766b61a2d6bb05262761b
Marine14 <marine.favier@hotmail.fr>
2019-10-24 15:24:38 +0200

    la vue principale ressemble à quelque chose

--- controler/affichePagePrincipale.php
+++ controler/affichePagePrincipale.php
@@ -15,8 +15,14 @@ if (isset($_GET['ref'])) {
 } else {
   // Pas de catégorie
   $categorie = 0;
-  $articles = $dao->firstN(10);
+  $articles = $dao->firstN(12);
 }
+
+  //test pour verifier que la variable article soit exact
+  // foreach($articles as $res){
+  //   var_dump($res->getRef());
+  //      }
+
 // Construction de la vue
 ////////////////////////////////////////////////////////////////////////////
 $view = new View();
@@ -24,7 +30,7 @@ $view = new View();
 // Passe les paramètres à la vue
 
 // Les articles
-$view->assign('articliees',$articles);
+$view->assign('articles',$articles);
 // La catégorie
 $view->assign('categorie',$categorie);
 
